425ee6db6633616293eb5eccc7d14e4194d8e100
Scot Newbury <scot@scotnewbury.com>
2023-08-12 01:15:09 -0400

    Complete code for Needle in a Haystack - code passed

--- needle-in-a-haystack.py
+++ needle-in-a-haystack.py
@@ -1,5 +1,9 @@
 def find_needle(haystack):
     # your code here
-
-
+    position = 0
+    for loop in haystack:
+        if (haystack[position] == "needle"):
+            return ("found the needle at position: " + str(position))
+        position += 1
 test_array = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
+find_needle (test_array)
