6efb92fe189e0b60e8bc74c42d1b9ad080fd5c2d
Scot Newbury <scot@scotnewbury.com>
2025-05-24 00:39:54 -0400

    Passed array with test data to fundtion

--- sum-arrays.py
+++ sum-arrays.py
@@ -1,4 +1,6 @@
-# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+# Write a function that takes an array of numbers and returns the sum of the numbers.
+# The numbers can be negative or non-integer. If the array does not contain any 
+# numbers then you should return 0.
 
 # Examples
 # Input: [1, 5.2, 4, 0, -1]
@@ -15,3 +17,10 @@
 # You cannot assume the size of the array.
 # You can assume that you do get an array and if the array is empty, return 0.
 
+def sum_array(a):
+    for i in range(len(a)):
+        print(a[i])
+    return(0)
+
+numbers = (1, 5.2, 4, 0, -1)
+print(sum_array(numbers))
